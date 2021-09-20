ffa48570e74aa6b64606d5f823f1bd0a7eaf6433
Alien Web Development <76794694+GoAlienWeb@users.noreply.github.com>
2021-09-20 07:51:56 -0500

    Update contact_me.js

--- contact_me.js
+++ contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "./contact_me.php",
+        url: "././contact-form-process.php",
         type: "POST",
         data: {
           name: name,
